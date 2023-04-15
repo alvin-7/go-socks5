4f962d3c42d2ead648074e272d5a31d54a3f7ffe
alvin-7 <zhuye.life@gmail.com>
2023-04-15 21:44:08 +0800

    add: user password example

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	socks5 "github.com/alvin-7/go-socks5"
+)
+
+func main() {
+	creds := socks5.StaticCredentials{
+		"username": "password",
+	}
+	conf := &socks5.Config{
+		AuthMethods: []socks5.Authenticator{&socks5.UserPassAuthenticator{
+			Credentials: creds,
+		}},
+		Logger: log.New(os.Stdout, "[socks5]", log.LstdFlags),
+	}
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SOCKS5 proxy on localhost port 8000
+	if err := server.ListenAndServe("tcp", "0.0.0.0:1080"); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module socks5
+
+go 1.19
+
+require (
+	github.com/alvin-7/go-socks5 v0.0.0-20160902184237-e75332964ef5
+	golang.org/x/net v0.9.0
+)
